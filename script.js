0e6759c5816b6c675f4cebf8e241d819d815481c
Kallio553 <88859266+Kallio553@users.noreply.github.com>
2024-10-23 22:31:57 +0200

    Create

---
+++ script.js
@@ -0,0 +1,19 @@
+function convertText() {
+    const string = document.getElementById("originalText").value;
+
+    const removeAccent = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const replaceSpecialCharWithDash = removeAccent.replace(/[^a-zA-Z0-9]/g, "-");
+    const removeUnwantedDashes = replaceSpecialCharWithDash.replace(/--+/g, '-');
+    const removeLastDash = removeUnwantedDashes.replace(/-$/, "");
+    const toLowerCase = removeLastDash.toLowerCase();
+    const result = toLowerCase;
+
+    document.getElementById("moddedText").value = result;
+}
+
+function copyToClipboard() {
+    const copyText = document.getElementById("moddedText");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+}
