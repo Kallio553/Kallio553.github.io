8ad66f7e427b8a519f07cc5bcdbffd5c6374c0cf
Kallio553 <88859266+Kallio553@users.noreply.github.com>
2025-08-27 19:11:00 +0200

    Update script.js
    Fix zero-width/invisible characters

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function convertText() {
     const string = document.getElementById("originalText").value;
 
-    const removeAccent = string.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const removeAccent = string
+        .normalize("NFD")
+        .replace(/[\u00AD\u200B-\u200D\u2060\uFEFF]/g, "")
+        .replace(/[\u0300-\u036f]/g, "");
     const replaceSpecialCharWithDash = removeAccent.replace(/[^a-zA-Z0-9]/g, "-");
     const removeUnwantedDashes = replaceSpecialCharWithDash.replace(/-+/g, '-');
     const removeStartOrEndDash = removeUnwantedDashes.replace(/^-|-$/g, "");
